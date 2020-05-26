f9e1c2ad2b3c77268d2c115a0c1bd133d0cdf5f5
SMK <idsiemu@gmail.com>
2020-05-26 21:51:14 +0900

    redux commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,31 @@ const plus = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const countModifier = (state = 0) => {
-    return state;
+const countModifier = (count = 0, action) => {
+    if(action.type === "ADD"){
+        return count + 1;
+    }else if(action.type === "MINUS"){
+        return count - 1;
+    }else{
+        return count;
+    }
 };
 
 const countStore = createStore(countModifier);
 
-console.log(countStore.getState());
+const onChange = () => {
+    number.innerText = countStore.getState();
+}
+
+const handlePlus = () => {
+    countStore.dispatch({type: "ADD"})
+}
+
+const handleMinus = () => {
+    countStore.dispatch({type: "MINUS"})
+}
+
+countStore.subscribe(onChange);
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
