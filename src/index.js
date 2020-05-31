41a0041fa872bdb3ee3ab76565b116e63a40e3cc
SMK <idsiemu@gmail.com>
2020-05-31 20:38:38 +0900

    리덕스 심화

--- src/index.js
+++ src/index.js
@@ -4,13 +4,47 @@ const plus = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+
+const add = 'ADD_TODO';
+const del = 'DELETE_TODO';
+
+const reducer = (state = [], action) => {
+    switch(action.type){
+        case add:
+            return [];
+        case del:
+            return [];
+        default:
+            return state;
+    }
+}
+
+const ulStore = createStore(reducer);
+
+const onSubmit = e => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value = "";
+    ulStore.dispatch({type:add, text: toDo});
+}
+
+form.addEventListener('submit', onSubmit);
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-    if(action.type === "ADD"){
-        return count + 1;
-    }else if(action.type === "MINUS"){
-        return count - 1;
-    }else{
-        return count;
+    switch (action.type){
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count -1;
+        default:
+            return count;
     }
 };
 
@@ -21,11 +55,11 @@ const onChange = () => {
 }
 
 const handlePlus = () => {
-    countStore.dispatch({type: "ADD"})
+    countStore.dispatch({type: ADD})
 }
 
 const handleMinus = () => {
-    countStore.dispatch({type: "MINUS"})
+    countStore.dispatch({type: MINUS})
 }
 
 countStore.subscribe(onChange);
